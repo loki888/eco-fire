17bfae8c770442cf47f4a23471c84b333d5821c4
loki_888 <loki_888@i.ua>
2017-09-01 15:35:57 +0300

    Add  fire risk calculation formula, it use functions from koeficients calculations pass them as a arguments

--- calculation/fireRiskCalculation.js
+++ calculation/fireRiskCalculation.js
@@ -2,11 +2,22 @@
 
 const express = require("express"),
 mongoose = require("mongoose"),
-Weather = require("./models/fire_risk-data-model");
+Weather = require("../models/fire_risk-data-model"),
+K_wind = require("../calculation/windCoeficient"),
+K_pertisipation = require("../calculation/patisipationCoeficient");
 //parseWeather = require('./parseWeatherReq');
 
 
+exports.fireRiskCalculation = function(){
+    let Kw = K_wind.windCoefCulc();
+    let Kp = K_pertisipation.pertisipationCoefCulc();
+    let T = temperature;
+    let d = dewpoint;
+    let fireRiskYesterday;
 
+    fireRisk = (Kp * fireRiskYesterday) + Kw * T * (T - d);
+    return fireRisk;
+};
 
 
 
