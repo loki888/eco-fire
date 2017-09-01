fbbe29ea2b71587794086f85f95b841c6b4ce7ce
loki_888 <loki_888@i.ua>
2017-09-01 14:55:10 +0300

    add calculation for wind and participation koeficients, create modules for each function as a Node module for export

---
+++ calculation/fireRiskCalculation.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express"),
+mongoose = require("mongoose"),
+Weather = require("./models/fire_risk-data-model");
+//parseWeather = require('./parseWeatherReq');
+
+
+
+
+
+
+module.exports = fireRiskCalculation;
---
+++ calculation/patisipationCoeficient.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// ===================== Розрахунок Коефіцієнту обліку опадів поточного дня (К) =====================================
+var     perticipetion = 2.8;
+
+exports.pertisipationCoefCulc = function(perticipetion){
+  let perticipetionToNumber = perticipetion - 0; // perticipetion приходить у вигляди строки, перетворюємо на числовий тип.
+  let     pertisipationCoeficient;
+  if (perticipetionToNumber === 0) {
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 1.0`);
+    return pertisipationCoeficient = 1.0;}
+  else if (0.05 < perticipetionToNumber && perticipetionToNumber < 0.9) {
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 0.8`);
+    return pertisipationCoeficient = 0.8;}
+  else if (0.91 < perticipetionToNumber && perticipetionToNumber < 2.9) {
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 0.6`);
+    return pertisipationCoeficient = 0.6;}
+  else if (2.91 < perticipetionToNumber && perticipetionToNumber < 5.9) {
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 0.4`);
+    return pertisipationCoeficient = 0.4;}
+  else if (5.91 < perticipetionToNumber && perticipetionToNumber < 14.9) {
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 0.2`);
+    return pertisipationCoeficient = 0.2;}
+  else if (14.91 < perticipetionToNumber){
+    console.log(`Сьогодні випало ${perticipetionToNumber} мм опадів, коефіцієнт опадів у цьому випадку становить: 0`);
+    return pertisipationCoeficient = 0;
+  }
+};
